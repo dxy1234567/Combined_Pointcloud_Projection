776675e00d091e0d6728faefe04241cae05a5cf1
dxy1234567 <dxy0020702@gmail.com>
2024-10-16 16:26:27 +0800

    bin2pcd

--- tools/bin_to_pcd.py
+++ tools/bin_to_pcd.py
@@ -1,23 +1,20 @@
 import numpy as np
 import open3d as o3d
 
-def read_kitti_bin_file(bin_path):
-    # 读取二进制文件
-    point_cloud = np.fromfile(bin_path, dtype=np.float32).reshape(-1, 4)
-    # 将点的x, y, z坐标提取出来
-    points = point_cloud[:, :3]
-    return points
-
-def get_point_cloud(bin_path):
-    # 将点转换为 Open3D 的 PointCloud 对象
-    points = read_kitti_bin_file(bin_path)
-
-    points_3d = o3d.geometry.PointCloud()
-    points_3d.points = o3d.utility.Vector3dVector(points)
-    return points_3d
+from others.KITTI.utils.functions import read_pcd_list, get_point_cloud
+from others.KITTI.utils.util import print_progress
 
 
-path_bin = ""
-path_output = ""
-pcd = get_point_cloud(path_bin)
-o3d.io.write_point_cloud(path_output, pcd)
+directory_pcd = "/data/KITTI/data_odometry_velodyne/dataset/sequences/00/velodyne"
+path_output = "/root/data/output/pcd/"
+
+pcd_list = read_pcd_list(directory_pcd)
+
+N = len(pcd_list)
+for i in range(N):
+    path_bin = pcd_list[i]
+    pcd = get_point_cloud(path_bin)
+    o3d.io.write_point_cloud(path_output, pcd)
+
+    print_progress(i, N)
+    
